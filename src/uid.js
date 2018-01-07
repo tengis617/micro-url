c62c3761222d141c564e94a84cb740ec17613a68
Tengis B <tengis617@users.noreply.github.com>
2018-01-07 17:27:46 +0700

    refactor to make it more readable

--- src/uid.js
+++ src/uid.js
@@ -1,22 +1,24 @@
-
 const CHARSET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const MAX_LENGTH = 8;
 
+// generate random int within the maximum range
 const generateRandomInt = max => Math.floor(Math.random() * (max + 1));
 
 const convertToBase62 = (i) => {
-  if (i === 0) { return '0'; }
+  if (i === 0) return '0';
   let s = '';
-  while (i > 0) {
-    s = CHARSET[i % 62] + s;
-    i = Math.floor(i / 62);
+  let remaining = i;
+  while (remaining > 0) {
+    const char = CHARSET[remaining % 62];
+    s = char + s;
+    remaining = Math.floor(remaining / 62);
   }
   return s;
 };
 
 const generate = () => {
-  // a random int within 62^8 = 218,340,105,584,896
   const base = CHARSET.length;
+  // a random int within 62^8 = 218,340,105,584,896
   const max = base ** MAX_LENGTH;
   const randomInt = generateRandomInt(max);
   const uid = convertToBase62(randomInt);
