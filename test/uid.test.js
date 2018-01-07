67428fd5f7075e3e4e3fad8482f3c121563f8737
Tengis B <tengis617@users.noreply.github.com>
2018-01-07 17:26:45 +0700

    added uid tests

--- test/uid.test.js
+++ test/uid.test.js
@@ -7,6 +7,24 @@ const { expect } = chai;
 
 describe('uid unit', () => {
   describe('generate()', () => {
+    it('should return z when base10 is 61', async () => {
+      const randomInt = 61;
+      const base62 = 'z';
+      const result = uid.convertToBase62(randomInt);
+      expect(result).to.equal(base62);
+    });
+    it('should return 1z when base10 is 123', async () => {
+      const randomInt = 123;
+      const base62 = '1z';
+      const result = uid.convertToBase62(randomInt);
+      expect(result).to.equal(base62);
+    });
+    it('should return 10 when base10 is 62', async () => {
+      const randomInt = 62;
+      const base62 = '10';
+      const result = uid.convertToBase62(randomInt);
+      expect(result).to.equal(base62);
+    });
     it('should return Jv when base10 is 1235', async () => {
       const randomInt = 1235;
       const base62 = 'Jv';
