36b6c8201317c1735a4f4b18cefee5739ff3b5be
Tengis B <tengis617@users.noreply.github.com>
2018-01-07 16:05:07 +0700

    refactor uid test

--- test/uid.test.js
+++ test/uid.test.js
@@ -3,17 +3,17 @@
 const uid = require('../src/uid');
 const chai = require('chai');
 
-const expect = chai.expect
+const { expect } = chai;
 
 describe('uid unit', () => {
   describe('generate()', () => {
-    it('should return ', async () => {
+    it('should return Jv when base10 is 1235', async () => {
       const randomInt = 1235;
       const base62 = 'Jv';
       const result = uid.convertToBase62(randomInt);
       expect(result).to.equal(base62);
     });
-    it('should return ', async () => {
+    it('should return W7w when base10 is 123500', async () => {
       const randomInt = 123500;
       const base62 = 'W7w';
       const result = uid.convertToBase62(randomInt);
